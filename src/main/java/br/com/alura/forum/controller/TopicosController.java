d9fc881e6b4a066915757bee9f442ec1b0cffb9e
Fabrícia Diniz <afabriciadiniz@gmail.com>
2019-07-12 18:19:44 -0300

    aula 5

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,7 +47,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) { //parâmetro recebido no corpo da requisição
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) { //parâmetro recebido no corpo da requisição
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
